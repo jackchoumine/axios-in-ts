cf950f918aed1f556f97e34a1568f33eac27a83f
jack <jackzhoumine@gmail.com>
2020-06-13 00:42:47 +0800

    refactor(types/index.ts xhr.ts): 把接口约束的url字段改为path
    把接口约束的url字段改为path

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,12 +2,12 @@
  * @Description: 类型定义入口
  * @Date: 2020-06-11 23:27:45
  * @Author: JackChouMine
- * @LastEditTime: 2020-06-11 23:33:21
+ * @LastEditTime: 2020-06-13 00:39:51
  * @LastEditors: JackChouMine
  */
 export type Method = 'get' | 'GET' | 'post' | 'delete' | 'put' | 'options' | 'head' | 'patch'
 export interface AxiosRequestConfig {
-  url: string
+  path: string
   method?: Method // 是字符串字面量类型约定值的范围
   data?: any
   params?: any
--- src/xhr.ts
+++ src/xhr.ts
@@ -2,15 +2,15 @@
  * @Description: 请求方法
  * @Date: 2020-06-11 23:36:07
  * @Author: JackChouMine
- * @LastEditTime: 2020-06-11 23:41:51
+ * @LastEditTime: 2020-06-13 00:39:32
  * @LastEditors: JackChouMine
  */
 import { AxiosRequestConfig } from './types'
 export default function xhr(config: AxiosRequestConfig): void {
   // 默认 GET 方法
-  const { url, data = null, method = 'get' } = config
+  const { path, data = null, method = 'get' } = config
   const request = new XMLHttpRequest()
   // NOTE xhr 要求方法大写
-  request.open(method.toUpperCase(), url, true) // 异步
+  request.open(method.toUpperCase(), path, true) // 异步
   request.send(data)
 }
